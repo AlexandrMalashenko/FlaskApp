efc79f8b319ae426bbbf3d4a29f47246a16ae912
AlexUnder <malashenko.470@gmail.com>
2019-04-08 22:34:01 +0300

    when user sign in insert in base record

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 from flask import render_template, flash, redirect, url_for
+from app import db
 from app import app
 from app.forms import LoginForm
+from app.models import User
 
 @app.route('/')
 @app.route('/index')
@@ -27,6 +29,9 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         flash('Login requested for user {}'.format(form.username.data))
-        print(form.username.data, form.password.data)
+        user = User(username=form.username.data, password_hash=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        print(User.query.all())
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
