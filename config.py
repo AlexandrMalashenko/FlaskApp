cdd6ec82c4d2fa765d99468053287bf83791853b
AlexUnder <malashenko.470@gmail.com>
2019-04-10 23:42:40 +0300

    Логирование ошибок

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
+from logging.handlers import RotatingFileHandler
 import os
+import logging
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
@@ -6,3 +9,16 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
                               'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+if not app.debug:
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240,
+                                       backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Microblog startup')
